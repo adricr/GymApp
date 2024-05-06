cd8ad99c946227f99257319366a7d15605e03b7a
Adrian <adriancorderorodriguez@gmail.com>
2024-05-06 19:43:17 +0100

    UserTest and Helper model for CSV ops created

---
+++ src/main/java/com/ac/helper/CSVFileOperations.java
@@ -0,0 +1,47 @@
+package com.ac.helper;
+
+import com.ac.model.Member;
+
+import java.io.*;
+
+public class CSVFileOperations {
+    private File file;
+
+    /**
+     * Class Constructor
+     * @param filepath the file name
+     */
+    public CSVFileOperations(String filepath){
+        this.file = new File(filepath);
+    }
+    public void writeMemberToCSV(Member member) throws IOException{
+        if(!file.exists()){
+            file.createNewFile();
+        }
+        try(PrintWriter writer = new PrintWriter(new FileWriter(file,true))){
+            writer.println(member.toString());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static int getLastIdFromCSV(File file) throws IOException{
+        if (!file.exists()) {
+            return -1;
+        }
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            String line;
+            int lastId = 0;
+            while((line = reader.readLine()) != null){
+                String [] parts = line.split(",");
+                lastId = Integer.parseInt((parts[0]));
+            }
+            return lastId + 1; //return last id plus one
+        }catch (IOException e){
+            e.printStackTrace();
+            return -1;
+        }
+
+    }
+
+}
--- src/main/java/com/ac/model/User.java
+++ src/main/java/com/ac/model/User.java
@@ -1,14 +1,27 @@
 package com.ac.model;
 
+import com.ac.helper.CSVFileOperations;
+
+import java.io.File;
+import java.io.IOException;
+
 public class User {
     //
-    private static int nextUserId;
+    private static int userNextId;
+
+    static {
+        try {
+            userNextId = CSVFileOperations.getLastIdFromCSV(new File("users.csv"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
     private String userId;
     private String username;
     private String password;
 
-    public User(String userId, String username, String password) {
-        this.userId = String.format("%07d", nextUserId++);
+    public User(String username, String password) {
+        this.userId = String.format("%07d", userNextId++);
         this.username = username;
         this.password = password;
     }
